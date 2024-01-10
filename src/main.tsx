4e62a80ddc4d12063c66d97c0ce888b0af5f5a64
hoangvu12 <hggaming91@gmail.com>
2024-01-11 00:18:31 +0700

    fix: browser throttle timers

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "hacktimer";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router, RouterProvider } from "@tanstack/react-router";
